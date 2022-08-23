52305e3b4c51e28130170f7a2d4aff77422252c4
icebox1234 <464752812@qq.com>
2022-08-23 09:47:32 +0800

    feat: complete test

--- test/test.spec.js
+++ test/test.spec.js
@@ -20,31 +20,8 @@ describe('assert', function () {
         e: 1
       }
     }
-    function getDeepth(obj) {
-      if (typeof obj !== 'object' || Array.isArray(obj)) {
-        return 0;
-      }
-      let deep = 0;
-      function _getDeepth(_obj) {
-        if (typeof _obj !== 'object') {
-          return 0;
-        }
-        let layerNodes = Object.keys(_obj);
-        let nextLayerDeep = 0;
-        for (let i = 0; i < layerNodes.length; ++i) {
-          if (!_obj.hasOwnProperty(layerNodes[i])) {
-            continue;
-          }
-          nextLayerDeep = Math.max(_getDeepth(_obj[layerNodes[i]]), nextLayerDeep);
-        }
-        deep = Math.max(deep, nextLayerDeep + 1);
-        return nextLayerDeep + 1;
-      }
-      _getDeepth(obj);
-      return deep;
-    }
     // 修改下面代码使得满足测试描述
-    assert.equal(getDeepth(a), getDeepth(b))
+    assert.deepEqual(a, b);
   })
 
   test('可以捕获并验证函数fn的错误', function () {
@@ -52,10 +29,6 @@ describe('assert', function () {
       xxx;
     }
     // 修改下面代码使得满足测试描述
-    try {
-      fn();
-    } catch (error) {
-      console.log(error);
-    }
+    assert.throws(fn, Error);
   })
 })
